af7269a9f9569e4f22ce5740ba265bbcf5c3c2cb
sasha <vivoce@hotmail.com>
2017-02-22 13:32:12 -0600

    Create migrations

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Store(models.Model):
+	user_id = models.ForeignKey(User)
+	store_name = models.CharField(max_length=30)
+	store_type = models.CharField(max_length=30)
+	store_hours = models.CharField(max_length=30)
+	store_address = models.CharField(max_length=100)
+	store_phone = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.store_name
+
+class Menu(models.Model):
+	store_id = models.ForeignKey(Store)
+	menu_type = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.menu_type
+
+class Menu_item(models.Model):
+	title = models.CharField(max_length=30)
+	description = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=6, decimal_places=3)
+	section = models.ManyToManyField(Menu)
+
+	def __str__(self):
+		return self.title
+
+
+
+
+
