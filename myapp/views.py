3177cd5f47d9928dce235015fb43413420bdd32e
sasha <vivoce@hotmail.com>
2017-02-22 19:29:49 -0600

    Add store details view

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Store
 from .models import Menu
 from .models import Menu_item
 
 def store_list(request):
 	stores = Store.objects.all().order_by('store_name')
-	return render(request, 'stores/store_list.html', {'stores': stores})
+	return render(request, 'shared/store_list.html', {'stores': stores})
+
+def store_detail(request, pk):
+    store = get_object_or_404(Store, pk=pk)
+    return render(request, 'shared/store_detail.html', {'store': store})
