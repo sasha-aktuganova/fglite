6f2cef2b9f380264255dd7b86fb4f46e679c9d3f
sasha <vivoce@hotmail.com>
2017-02-22 20:16:49 -0600

    Add form for a new store

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.store_list, name='store_list'),
     url(r'^store/(?P<pk>\d+)/$', views.store_detail, name='store_detail'),
+    url(r'^store/new/$', views.store_new, name='store_new'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Store
 from .models import Menu
 from .models import Menu_item
+from .forms import StoreForm
 
 def store_list(request):
 	stores = Store.objects.all().order_by('store_name')
@@ -10,3 +11,7 @@ def store_list(request):
 def store_detail(request, pk):
     store = get_object_or_404(Store, pk=pk)
     return render(request, 'shared/store_detail.html', {'store': store})
+
+def store_new(request):
+    form = StoreForm()
+    return render(request, 'shared/store_edit.html', {'form': form})
